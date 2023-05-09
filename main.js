bb3020b298be46b84ccf86f344194d47b4fe1684
luis Corcega <kikecorcega@gmail.com>
2023-05-08 20:18:11 -0500

    Corcega Master 1

---
+++ main.js
@@ -0,0 +1,24 @@
+console.log("MAIN")
+
+var a = "A";
+let b = 1;
+const c =0;
+
+//LISTAS
+var e1="manzana";
+var e2="naranja";
+var e3="frutilla";
+const lista1 = [e1,e2,e3]
+
+console.log(lista1)
+
+lista1.push(aguacate);
+
+
+const lista2 = {
+    nombre: "Luis Corcega",
+    edad: "20",
+    ciudad: "Guayaquil",
+}
+
+console.log(lista2)
